f7cbbee6bac41771e0ba2c0f6dbbe77b9ad89c51
AlexeiZaits <zaykuh@gmail.com>
2025-01-21 01:04:54 +0500

    upd: server

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -4,18 +4,22 @@ import { initDB } from "./models";
 import authRoutes from "./routes/authRoutes";
 import postRoutes from "./routes/postRoutes";
 import cors from 'cors';
+import cookieParser from "cookie-parser"
 
 dotenv.config();
 
 const app = express();
 
+app.use(cookieParser())
 app.use(cors({
-  origin: 'http://localhost:5173',
+  //разрешить всем
+  origin: function (origin, callback) {
+    callback(null, true);
+  },
   methods: ['GET', 'POST', 'PUT', 'DELETE'], 
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
 }));
-
 app.use(express.json());
 
 app.use("/auth", authRoutes);
