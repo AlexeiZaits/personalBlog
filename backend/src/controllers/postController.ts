d888c22bf7d769fbbc3e8e574fcde00d8ec21edc
AlexeiZaits <zaykuh@gmail.com>
2025-01-21 01:02:58 +0500

    controllers: add post, user

---
+++ backend/src/controllers/postController.ts
@@ -0,0 +1,138 @@
+import mime from "mime";
+import { Post } from "../models";
+import { yandexService } from "../service/yandexService";
+
+
+class PostController {
+
+    async createPost(req: any, res: any){
+        try {
+            const { content } = req.body;
+            const media = req.files
+            const {id, lastname, firstname} = req.user;
+            const userId = id
+            console.log(content, media.length)
+            if (!content && (!media || !Array.isArray(media) || media.length === 0)) {
+                return res.status(400).json({ error: "Content is required" });
+            }
+
+            const bucketName = 'personal-blog';
+
+            const mediaUrls = [];
+            for (const [index, file] of media.entries()) {
+                const fileExtension = file.originalname.split('.').pop(); // Получение расширения
+                const fileName = `media/${userId}/${Date.now()}_${index}.${fileExtension}`; // Уникальное имя
+                const fileUrl = await yandexService.uploadFileToYandex(file.buffer, bucketName, fileName); // Загрузка
+                mediaUrls.push(fileUrl); // Сохраняем ссылку на загруженный файл
+            }
+            
+            // Сохранение поста в базе данных
+            const post = await Post.create({ content, mediaUrls: mediaUrls, userId, lastname, firstname });
+            return res.status(201).json(post);
+            } catch (err) {
+            console.error('Ошибка создания поста:', err);
+            return res.status(500).json({ error: "Ошибка в создании новой записи" });
+            }
+    }
+    
+    async getPosts(req: any, res: any){
+        try {
+            const posts = await Post.findAll({ order: [["createdAt", "DESC"]] });
+            res.json(posts);
+        } catch (err) {
+            res.status(500).json({ error: "Ошибка в получении всех записей" });
+        }
+    }
+    
+    async deletePost(req: any, res: any){
+        try{
+            const { id } = req.params;
+            const userId = req.user.id;
+            
+            const post = await Post.findOne({ where: { id, userId } });
+            if (!post) {
+                return res.status(404).json({ error: "Post not found or unauthorized" });
+            }
+            
+            const mediaUrls = post.mediaUrls;
+    
+            if (mediaUrls && mediaUrls.length > 0) {
+                const fileNames = mediaUrls.map(url => {
+                    const urlParts = url.split('media');
+                    return "media"+urlParts[1];
+                });
+                
+                await yandexService.deleteFilesFromYandex(fileNames);
+            }
+            
+            await post.destroy();
+            
+            res.json({ message: "Post and media deleted successfully" });
+        } catch (err) {
+            console.error(err);
+            res.status(500).json({ error: "Error deleting post" });
+        }
+    }
+
+    async changePost (req: any, res: any){
+        try {
+            const { id } = req.params;
+            const {content, mediaUrls} = req.body
+            const userId = req.user.id;
+            const media = req.files
+            const post = await Post.findOne({ where: { id, userId } });
+            const parseMediaUrls = JSON.parse(mediaUrls)
+            console.log(parseMediaUrls)
+            if (!post) {
+                return res.status(400).json({ error: "Post not found" });
+            }
+            
+            const bucketName = 'personal-blog';
+            const mediaUrlsYandex = [];
+            try {
+                for (const [index, file] of media.entries()) {
+                    const fileExtension = file.originalname.split('.').pop(); // Получение расширения
+                    const fileName = `media/${userId}/${Date.now()}_${index}.${fileExtension}`; // Уникальное имя
+                    const fileUrl = await yandexService.uploadFileToYandex(file.buffer, bucketName, fileName); // Загрузка
+                    console.log({fileUrl: fileUrl});
+                    mediaUrlsYandex.push(fileUrl); // Сохраняем ссылку на загруженный файл
+                }
+                
+            } catch (error) {
+                console.log("Ошибка в загрузке файлов")
+            }
+
+            post.content = content
+            if (post.mediaUrls && post.mediaUrls.length > 0) {
+                const deletePosts:string[] = [];
+                post.mediaUrls.forEach((item) => {
+                    if (!parseMediaUrls || !parseMediaUrls.includes(item)){
+                        const urlParts = item.split('media');
+                        deletePosts.push("media"+urlParts[1])
+                    }
+                })
+                if (deletePosts.length > 0){
+                    await yandexService.deleteFilesFromYandex(deletePosts as string[]);
+                }
+            }
+            
+            const safeParseMediaUrls = Array.isArray(parseMediaUrls) ? parseMediaUrls : [];
+            const safeMediaUrlsYandex = Array.isArray(mediaUrlsYandex) ? mediaUrlsYandex : [];
+            post.mediaUrls = [...safeParseMediaUrls, ...safeMediaUrlsYandex];
+            if (!post.mediaUrls && !post.content){
+                await post.destroy()
+                res.json({message: "Post and media deleted successfully"});
+            } else {
+                await post.save();
+                res.json(post);
+            }
+            
+            
+        } catch (error) {
+            res.status(500).json({ error: "Error change post" });
+        }
+    }
+    
+}
+
+export const postController = new PostController();
---
+++ backend/src/controllers/userController.ts
@@ -0,0 +1,58 @@
+import { userService } from "../service/user-service";
+import { v4 as uuidv4 } from 'uuid';
+
+class UserController {
+    async registration (req: any, res: any, next: any){
+        try {
+            const {login, password, lastname, firstname} = req.body;
+            console.log()
+            const uniqueId = uuidv4();
+            const userData = await userService.registration({login: login, password: password, lastname: lastname, firstname: firstname, id: uniqueId})
+            
+            res.cookie('refreshToken', userData.tokens.refreshToken, {maxAge: 30*24*60*60*1000, httpOnly: true})
+            return res.json(userData)
+            
+        } catch (error) {
+            return res.status(500).json({message: "Ошибка при регистрации пользователя"})
+        }
+    }
+
+    async login (req: any, res: any, next: any){
+        try {
+            const {login, password} = req.body;
+            const userData = await userService.login(login, password);
+            res.cookie('refreshToken', userData.tokens.refreshToken, {maxAge: 30*24*60*60*1000, httpOnly: true})
+            return res.json(userData)
+            
+        } catch (error) {
+            return res.status(500).json({message: "Неправильно введён логин или пароль"})
+        }
+    }
+
+    async logout (req: any, res: any, next: any){
+        try {
+            const {refreshToken} = req.cookies
+            const token = await userService.logout(refreshToken);
+            res.clearCookie('refreshToken');
+            return res.json({message: "Успешно вышел с аккаунта"});
+            
+        } catch (error) {
+            return res.status(500).json({message: "Неправильно введён логин или пароль"})
+        }
+    }
+
+    async refresh (req: any, res: any, next: any){
+        try {
+            const {refreshToken} = req.cookies;
+            console.log(refreshToken)
+            const userData = await userService.refresh(refreshToken);
+            res.cookie('refreshToken', userData.tokens.refreshToken, {maxAge: 30*24*60*60*1000, httpOnly: true})
+            return res.json(userData)
+            
+        } catch (error) {
+            return res.status(500).json({message: "Неправильно введён логин или пароль"})
+        }
+    }
+}
+
+export const userController = new UserController()
