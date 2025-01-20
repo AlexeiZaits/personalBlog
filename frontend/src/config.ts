50f3e230993bf1ba74610eec4099068008214c5c
AlexeiZaits <zaykuh@gmail.com>
2025-01-21 01:07:58 +0500

    upd config

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,8 +1,10 @@
-export const API_PORT = import.meta.env.VITE_BACKEND_PORT;
-export const API_HOST = import.meta.env.VITE_BACKEND_HOST;
-export const API_URL = `http://localhost:3000/`;
+export const API_URL = import.meta.env.VITE_API_URL;
 
-export const authUser = API_URL + `auth/login`;
-export const regUser = API_URL + `auth/register`;
-export const logout = API_URL + `auth/logout`;
-export const resresh = API_URL + `auth/user`;
+export const authUser = `auth/login`;
+export const regUser = `auth/register`;
+export const logout = `auth/logout`;
+export const resresh = `auth/refresh`;
+export const deletePost = (id: number) => `posts/${id}`;
+export const addPost = "posts";
+export const changePost = (id: number) => `posts/${id}`;
+export const getPosts = "posts";
