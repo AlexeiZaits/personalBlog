0932636135c17e98070882252f278abb74f870c1
AlexeiZaits <zaykuh@gmail.com>
2025-01-21 01:06:16 +0500

    routes: upd auth, post

--- backend/src/routes/authRoutes.ts
+++ backend/src/routes/authRoutes.ts
@@ -1,85 +1,12 @@
 import express from "express";
-import jwt from "jsonwebtoken";
-import { RefreshToken, User } from "../models";
+import { userService } from "../service/user-service";
+import { userController } from "../controllers/userController";
 
 const router = express.Router();
 
-export const generateAccessToken = (userId: number): string => {
-  if (!process.env.JWT_SECRET) {
-    throw new Error("JWT_SECRET is not defined");
-  }
-  return jwt.sign({ id: userId }, process.env.JWT_SECRET, { expiresIn: "15m" });
-};
-
-export const generateRefreshToken = async (userId: number): Promise<string> => {
-  if (!process.env.JWT_SECRET) {
-    throw new Error("JWT_SECRET is not defined");
-  }
-  
-  const token = jwt.sign({ id: userId }, process.env.JWT_SECRET, { expiresIn: "7d" });
-
-  await RefreshToken.create({
-    token,
-    userId,
-    expiresAt: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
-  });
-
-  return token;
-};
-
-router.post("/register", async (req, res) => {
-  console.log(req.body)
-  try {
-    const { username, password } = req.body;
-
-    const user = await User.create({ username, password });
-    console.log(user)
-    const accessToken = generateAccessToken(user.id);
-    const refreshToken = await generateRefreshToken(user.id);
-
-    res.json({ message: "User registered", accessToken, refreshToken });
-  } catch (err: any) {
-    res.status(400).json({ error: err.message });
-  }
-});
-
-router.post("/login", async (req: any, res: any) => {
-  try {
-    const { username, password } = req.body;
-
-    const user = await User.findOne({ where: { username } });
-    if (!user) {
-      return res.status(401).json({ error: "Invalid credentials" });
-    }
-
-    const isValidPassword = await user.comparePassword(password);
-    if (!isValidPassword) {
-      return res.status(401).json({ error: "Invalid credentials" });
-    }
-
-    const accessToken = generateAccessToken(user.id);
-    const refreshToken = await generateRefreshToken(user.id);
-
-    res.json({ message: "Login successful", accessToken, refreshToken });
-  } catch (err: any) {
-    res.status(400).json({ error: err.message });
-  }
-});
-
-router.post("/logout", async (req: any, res: any) => {
-    try {
-      const { refreshToken } = req.body;
-  
-      if (!refreshToken) {
-        return res.status(400).json({ error: "Refresh token is required" });
-      }
-      
-      await RefreshToken.destroy({ where: { token: refreshToken } });
-  
-      res.json({ message: "User logged out" });
-    } catch (err: any) {
-      res.status(400).json({ error: err.message });
-    }
-});
+router.post("/register", userController.registration);
+router.post("/login", userController.login);
+router.post("/logout", userController.logout);
+router.get("/refresh", userController.refresh)
 
 export default router;
--- backend/src/routes/postRoutes.ts
+++ backend/src/routes/postRoutes.ts
@@ -1,74 +1,25 @@
 import express from "express";
-import { Post } from "../models";
 import { authMiddleware } from "../middlewares/authMiddleware";
+import { postController } from "../controllers/postController";
+import multer from 'multer';
+
+// Настройка хранилища
+const storage = multer.memoryStorage(); // Хранит файлы в памяти как Buffer
+const upload = multer({ storage });     // Конфигурация Multer
 
 const router = express.Router();
 
 // Создать новую запись
-router.post("/", authMiddleware, async (req: any, res: any) => {
-  try {
-    const { content, mediaUrl } = req.body;
-    const userId = req.user.id; // ID из JWT токена
-
-    if (!content) {
-      return res.status(400).json({ error: "Content is required" });
-    }
-
-    const post = await Post.create({ content, mediaUrl, userId });
-    res.status(201).json(post);
-  } catch (err) {
-    res.status(500).json({ error: "Ошибка в создании новой записи" });
-  }
-});
+router.post("/", authMiddleware, upload.array('media'), postController.createPost);
 
 // Получить все записи
-router.get("/", async (req, res) => {
-  try {
-    const posts = await Post.findAll({ order: [["createdAt", "DESC"]] });
-    res.json(posts);
-  } catch (err) {
-    res.status(500).json({ error: "Ошибка в получении всех записей" });
-  }
-});
+router.get("/", postController.getPosts);
 
 // Редактировать запись
-router.put("/:id", authMiddleware, async (req: any, res: any) => {
-  try {
-    const { id } = req.params;
-    const { content, mediaUrl } = req.body;
-    const userId = req.user.id; // ID из JWT токена
-
-    const post = await Post.findOne({ where: { id, userId } });
-    if (!post) {
-      return res.status(404).json({ error: "Post not found or unauthorized" });
-    }
-
-    post.content = content || post.content;
-    post.mediaUrl = mediaUrl || post.mediaUrl;
-
-    await post.save();
-    res.json(post);
-  } catch (err) {
-    res.status(500).json({ error: "Ошибка в редактировании записи" });
-  }
-});
+router.put("/:id", authMiddleware, upload.array('media'), postController.changePost);
 
 // Удалить запись
-router.delete("/:id", authMiddleware, async (req: any, res: any) => {
-  try {
-    const { id } = req.params;
-    const userId = req.user.id; // ID из JWT токена
+router.delete("/:id", authMiddleware, postController.deletePost);
 
-    const post = await Post.findOne({ where: { id, userId } });
-    if (!post) {
-      return res.status(404).json({ error: "Post not found or unauthorized" });
-    }
-    
-    await post.destroy();
-    res.json({ message: "Post deleted" });
-  } catch (err) {
-    res.status(500).json({ error: "Ошибка в удалении поста" });
-  }
-});
 
 export default router;
