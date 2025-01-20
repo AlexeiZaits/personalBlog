91f1afcaa031c35a4aeef259c5776627a1fc856a
AlexeiZaits <zaykuh@gmail.com>
2025-01-21 01:05:19 +0500

    middlewares: upd auth

--- backend/src/middlewares/authMiddleware.ts
+++ backend/src/middlewares/authMiddleware.ts
@@ -1,24 +1,30 @@
 import { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
+import { tokenService } from "../service/token-service";
 
 interface AuthRequest extends Request {
   user?: { id: number };
 }
 
-export const authMiddleware = (req: AuthRequest, res: Response, next: NextFunction): void => {
-  const authHeader = req.headers.authorization;
-
-  if (!authHeader || !authHeader.startsWith("Bearer ")) {
-    res.status(401).json({ error: "Unauthorized" });
-    return;
-  }
-
-  const token = authHeader.split(" ")[1];
-
+export const authMiddleware = (req:any, res: Response, next: NextFunction): void => {
   try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET!) as { id: number };
-    req.user = { id: decoded.id };
-    next(); // Завершаем middleware вызовом next()
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+      res.status(401).json({ error: "Пользователь не авторизован" });
+      return;
+    }
+  
+    const token = authHeader.split(" ")[1];
+  
+  
+    const userData = tokenService.validateAccessToken(token)
+    if (!userData){
+      res.status(401).json({ error: "Пользователь не авторизован" });
+      return;
+    }
+    req.user = userData
+    next();
   } catch (err) {
     res.status(401).json({ error: "Invalid token" });
   }
